53f5c54d86600686aa18acb190250916802fdab1
marbl <47604239+marbl3@users.noreply.github.com>
2022-05-30 10:29:11 +0200

    Update __manifest__.py

--- marbl_demo/__manifest__.py
+++ marbl_demo/__manifest__.py
@@ -27,7 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'data/crm_data.xml'
+        'data/crm_data.xml',
         'data/contact_demo.xml'
     ],
     # only loaded in demonstration mode
